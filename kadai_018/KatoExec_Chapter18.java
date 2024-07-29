fe4f6714010029e259cfc0311b7dbf31541ad67e
mait0521 <mtakahashi.0521@gmail.com>
2024-07-29 17:01:47 +0900

    課題の提出

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,18 @@
+package kadai.kadai_018;
+
+public class KatoExec_Chapter18 {
+public static void main(String[] args) {
+		
+		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		taro.execIntroduce();
+		
+		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+		hanako.setGivenName();
+		hanako.execIntroduce();
+		
+		KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+		ichiro.setGivenName();
+		ichiro.execIntroduce();
+	}
+
+}
---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,24 @@
+package kadai.kadai_018;
+
+abstract public class Kato_Chapter18 {
+	public String familyName = "加藤";
+	public String givenName = "";
+	public String address = "東京都中野区〇×";
+	
+    public Kato_Chapter18() {
+    }	
+	
+	public void commonIntroduce() {
+		System.out.println("名前は" + familyName + givenName + "です");
+		System.out.println("住所は" + address + "です");
+	}
+	
+	abstract public void eachIntroduce();
+	
+	public void execIntroduce() {
+		commonIntroduce();
+		eachIntroduce();
+		System.out.println("");
+	}
+
+}

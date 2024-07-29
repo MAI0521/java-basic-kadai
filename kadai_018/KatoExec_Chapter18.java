58398a887254c08452872aae52940695470b1bfd
mait0521 <mtakahashi.0521@gmail.com>
2024-07-29 19:21:06 +0900

    課題の再提出

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -4,6 +4,7 @@ public class KatoExec_Chapter18 {
 public static void main(String[] args) {
 		
 		KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+		taro.setGivenName();
 		taro.execIntroduce();
 		
 		KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
